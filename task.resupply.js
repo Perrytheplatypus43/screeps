d419a2a530a8b66e0e1d4cf08fa594b26cc85306
zmarkham <zmarkham@outlook.com>
2016-12-29 20:24:10 +0000

    Fixed resupply not working when trying to find a dropped energy source rather than a container

--- task.resupply.js
+++ task.resupply.js
@@ -11,16 +11,22 @@ let resupply = {
 
         let energyStore = utils.findNearest(creep, FIND_STRUCTURES, (struct) =>  struct.structureType == STRUCTURE_CONTAINER && struct.store.energy > 100);
 
-        if (!energyStore)
-        {
-            energyStore = utils.findNearest(creep, FIND_DROPPED_ENERGY);
-        }
-
         if (energyStore)
         {
             if (creep.withdraw(energyStore, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
             {
                 creep.moveTo(energyStore);
+            }
+        }
+        else
+        {
+            energyStore = utils.findNearest(creep, FIND_DROPPED_ENERGY);
+            if (energyStore)
+            {
+                if (creep.pickup(energyStore) == ERR_NOT_IN_RANGE)
+                {
+                    creep.moveTo(energyStore);
+                }
             }
         }
     }

06490484f656994825a20b5775378447d7d33018
zmarkham <zmarkham@outlook.com>
2016-12-29 20:18:31 +0000

    Changed resupply filter to look for containers with at least 100 energy in, if this search yields no results, then look for dropped energy instead

--- task.resupply.js
+++ task.resupply.js
@@ -9,10 +9,19 @@ let resupply = {
 
     resupply: function(creep) {
 
-        let energyStore = utils.findNearest(creep, FIND_STRUCTURES, (struct) =>  struct.structureType == STRUCTURE_CONTAINER);
+        let energyStore = utils.findNearest(creep, FIND_STRUCTURES, (struct) =>  struct.structureType == STRUCTURE_CONTAINER && struct.store.energy > 100);
 
-        if (creep.withdraw(energyStore, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(energyStore);
+        if (!energyStore)
+        {
+            energyStore = utils.findNearest(creep, FIND_DROPPED_ENERGY);
+        }
+
+        if (energyStore)
+        {
+            if (creep.withdraw(energyStore, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+            {
+                creep.moveTo(energyStore);
+            }
         }
     }
 };

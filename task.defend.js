a7a5f80f87b25f8b7a65ed67f8b0a7240bcb16ff
zmarkham <zmarkham@outlook.com>
2016-12-30 23:14:52 +0000

    Fixed erroneous line left in

--- task.defend.js
+++ task.defend.js
@@ -21,8 +21,6 @@ let defend = {
         }
         else
         {
-            let pos = creep.pos.findClosestByRange(FIND_FLAGS, {filter: (flag) => flag.name.include("ArcherSpot")}).pos;
-
             let occupiedArcherSpots = _.filter(Game.creeps, (creep) => creep.memory.role == 'defender').map((el) => el.memory.archerSpot);
             let closestFlag = creep.pos.findClosestByRange(FIND_FLAGS, (flag) => flag.name.includes("ArcherSpot") && occupiedArcherSpots.indexOf(flag.pos) == -1);
             if (closestFlag) creep.memory.archerSpot = closestFlag.pos;

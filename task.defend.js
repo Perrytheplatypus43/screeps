4162bfcc9c44675d7664976041e083d098df0209
zmarkham <zmarkham@outlook.com>
2016-12-30 23:14:16 +0000

    Set up logic for archers to position themselves on flags

--- task.defend.js
+++ task.defend.js
@@ -9,12 +9,26 @@ let defend = {
 
     defend: function(creep) {
 
-        let target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (target) {
-            if (creep.rangedAttack(target) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
+        if (creep.memory.archerSpot)
+        {
+            creep.moveTo(creep.memory.archerSpot);
+
+            let target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            if (target) {
+                creep.rangedAttack(target);
             }
+
         }
+        else
+        {
+            let pos = creep.pos.findClosestByRange(FIND_FLAGS, {filter: (flag) => flag.name.include("ArcherSpot")}).pos;
+
+            let occupiedArcherSpots = _.filter(Game.creeps, (creep) => creep.memory.role == 'defender').map((el) => el.memory.archerSpot);
+            let closestFlag = creep.pos.findClosestByRange(FIND_FLAGS, (flag) => flag.name.includes("ArcherSpot") && occupiedArcherSpots.indexOf(flag.pos) == -1);
+            if (closestFlag) creep.memory.archerSpot = closestFlag.pos;
+        }
+
+
     }
 };
 
